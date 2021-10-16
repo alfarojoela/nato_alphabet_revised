efa584d15bb448250caa2e7d2b6550d80ad410bc
Joel Alfaro <alfarojoela@outlook.com>
2021-10-15 17:04:04 -0700

    examples on try and except catch.  with nato

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+#set up try and function incorrectly.  recursion limit reached.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+
+    except KeyError:
+        print("Only letters allowed.  No numbers.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
